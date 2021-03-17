35b926db3dbc25ce368eb3f833a4c003a31c21fa
Deguriii <40736396+Deguriii@users.noreply.github.com>
2021-03-17 23:08:07 +0900

    updated App js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Header from './Containers/Header.js';
 import Category from './Containers/Category.js'
 import Sidebar from './Containers/Sidebar.js'
 import Toast from "./Containers/Toast.js"
+import { Router } from 'react-router';
 
+<<<<<<< Updated upstream
 class App extends Container{
   render() {
+=======
+function App() {
+>>>>>>> Stashed changes
     return (
-      <div className="App">
-        <Header></Header>
-        <Category></Category>
-        <Sidebar></Sidebar>
-        <Toast></Toast>
-      </div>
-
-    );
+      <React.Fragment>
+        <Router>
+          <div className="App">
+          <Header></Header>
+          </div>
+        </Router>
+      </React.Fragment>
+    )
   }
-}
+
 
 export default App;
