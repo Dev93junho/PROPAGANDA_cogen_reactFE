e1a692f56221ee35d1c4a86fb7f703d7ba78b979
junho <jhshin9345@gmail.com>
2021-03-26 13:10:39 +0900

    Updated Card.js in Contents.js

--- src/Containers/Contents.js
+++ src/Containers/Contents.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from './Card';
 import "./css/Contents.css";
 import { RiThumbUpLine } from "react-icons/ri"
 import { GrCircleInformation } from "react-icons/gr"
@@ -9,10 +10,45 @@ class Contents extends React.Component {
             <div className="contents">
                 <div className="contents-header">
                     <RiThumbUpLine size="24" />
-                    <span className={this.props.name ? 'contents head' : 'class'}>
- 
-                    </span>
-                    <GrCircleInformation size="24" />
+                    <h4>Top Post</h4>
+                    
+                    <GrCircleInformation size="24" color="#F67600"/>
+                </div>
+                <div className="contents-body">
+                    <Card />
+                </div>
+                <div className="contents-header">
+                    <RiThumbUpLine size="24" />
+                    <h4>Subject</h4>
+                    <GrCircleInformation size="24" color="#F67600"/>
+                </div>
+                <div className="contents-body">
+                    <Card />
+                </div>
+
+                <div className="contents-header">
+                    <RiThumbUpLine size="24" />
+                    <h4>#tag</h4>
+                    <GrCircleInformation size="24" color="#F67600"/>
+                </div>
+                <div className="contents-body">
+                    <Card />
+                </div>
+                <div className="contents-header">
+                    <RiThumbUpLine size="24" />
+                    <h4>Series</h4>
+                    <GrCircleInformation size="24" color="#F67600"/>
+                </div>
+                <div className="contents-body">
+                    <Card />
+                </div>
+                <div className="contents-header">
+                    <RiThumbUpLine size="24" />
+                    <h4>User</h4>
+                    <GrCircleInformation size="24" color="#F67600"/>
+                </div>
+                <div className="contents-body">
+                    <Card />
                 </div>
             </div>
 
--- src/Containers/card.js
+++ src/Containers/card.js
@@ -2,20 +2,29 @@
 모든것을 통합관리하는 조건부 렌더링을 적용합니다.
  */ 
 import React from "react";
+import "./css/card.css"
 
-function Card({title, imgUrl, body}){
+function Card({subject, title, imgUrl, body}){
     return (
-        <div className="container-card">
+        <div className="card_article_big">
             <div className="card-Img">
                 <img src={imgUrl} alt='' />
             </div>
+            <div className="card-subject">
+                <h4>{subject}</h4>
+            </div>
             <div className="card-title">
-                <h3>{title}</h3>
+                <h4>{title}</h4>
             </div>
             <div className="card-body">
                 <p>{body}</p>
             </div>
-            <div className="info"></div>
+            <div className="card-info">
+                <span>댓글 </span>
+                <span>origin </span>
+                <span>Response </span>
+                
+            </div>
                 
         </div>
     )
