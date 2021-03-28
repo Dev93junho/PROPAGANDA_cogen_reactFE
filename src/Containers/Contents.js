3409d826b8143cf8b16677cf7cb241ac3bf43013
Deguriii <40736396+MinTpie30@users.noreply.github.com>
2021-03-28 10:53:50 +0900

    Update Contents.js

--- src/Containers/Contents.js
+++ src/Containers/Contents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './Card';
+import Card from './card';
 import "./css/Contents.css";
 import { RiThumbUpLine } from "react-icons/ri"
 import { GrCircleInformation } from "react-icons/gr"
