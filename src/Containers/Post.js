08bd55dd42f1754e6af9b2f44d1c1831c3052882
junho <jhshin9345@gmail.com>
2021-04-08 17:22:32 +0900

    Updated Post Tab

--- src/Containers/Post.js
+++ src/Containers/Post.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import './css/Post.css';
+import SubjectTagCard from './SubjectTagCard';
+import ListArticle from './ListArticle';
+
+
 
 export default function Post() {
     return (
         <div className="PostContainer">
-            1
+            <div className="top">
+                <SubjectTagCard />
+                <SubjectTagCard />
+                <SubjectTagCard />
+            </div>
+            <div className="mid">
+                <ul className="selectSubject">
+                    <li>인문</li>
+                    <li>자연과학</li>
+                    <li>경제경영</li>
+                    <li>문화</li>
+                    <li>예술</li>
+                    <li>스포츠</li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="head">
+                    <span>공개글</span>
+                    <span>더보기</span>
+                </div>
+                <div className="publishList">
+                    <ListArticle />
+                </div>
+
+            </div>
+
         </div>
 
     );
